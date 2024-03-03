b1e14924b38a1790dcb5e34480a34fe211400a54
arvindsrathore <arvindsingh_21289@aitpune.edu.in>
2024-03-03 22:26:30 +0530

    Added userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,44 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required:[true,'Please tell us your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    bio: {
+        type: String
+    },
+    skills: {
+        type: [String]
+    },
+    skillsToLearn: {
+        type: [String],
+        required: [false]
+    },
+    skillsToTeach: {
+        type: [String],
+        required: [false]
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
