2584bb589f86e16ffa32dee92917a1857d677d4c
arvindsrathore <arvindsingh_21289@aitpune.edu.in>
2024-03-05 00:55:58 +0530

    Data Modelling, M2M Referencing

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,18 +5,20 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 const User = require('./../models/userModel')
+const Skills = require('./../models/skillsModel')
 
 exports.signup = async(req,res,next) => {
     const newUser = await User.create({
         username: req.body.username,
         email: req.body.email,
         bio: req.body.bio,
-        skills: req.body.skills,
+        userSkills: req.body.userSkills,
         skillsToLearn: req.body.skillsToLearn,
         skillsToTeach: req.body.skillsToTeach,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     })
+    
     res.status(201).json({
         status: 'success',
         userdata: {
