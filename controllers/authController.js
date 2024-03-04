1a56ede35b7737e1666d57a803b725f01c9b4b1e
arvindsrathore <arvindsingh_21289@aitpune.edu.in>
2024-03-04 12:12:28 +0530

    Signup with password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,3 +3,24 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+
+const User = require('./../models/userModel')
+
+exports.signup = async(req,res,next) => {
+    const newUser = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        bio: req.body.bio,
+        skills: req.body.skills,
+        skillsToLearn: req.body.skillsToLearn,
+        skillsToTeach: req.body.skillsToTeach,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    })
+    res.status(201).json({
+        status: 'success',
+        userdata: {
+            newUser
+        } 
+    })
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const userController = require('./../controllers/userController')
+const authController = require('./../controllers/authController')
 
 const router = express.Router();
 
+router.post('/signup',authController.signup);
+
+module.exports = router;
