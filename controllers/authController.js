0b002d8b795ebb469bb8769e68c28df782c3e2db
Divya Prakash Jha <prakashdivya0607@gmail.com>
2024-03-05 00:14:42 +0530

    Added signup, login, logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,42 @@
 const crypto = require('crypto');
-
+const {promisify} = require('util');
 const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
-const User = require('./../models/userModel')
+const User = require('./../models/userModel');
 
-exports.signup = async(req,res,next) => {
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    });
+};
+  
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  
+    res.cookie('jwt', token, cookieOptions);
+  
+    // Remove password from output
+    user.password = undefined;
+  
+    res.status(statusCode).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
+};
+
+exports.signup = catchAsync(async(req,res,next) => {
     const newUser = await User.create({
         username: req.body.username,
         email: req.body.email,
@@ -17,10 +47,83 @@ exports.signup = async(req,res,next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     })
-    res.status(201).json({
-        status: 'success',
-        userdata: {
-            newUser
-        } 
-    })
-}
+    // res.status(201).json({
+    //     status: 'success',
+    //     userdata: {
+    //         newUser
+    //     } 
+    // })
+    createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      return next(new AppError('Please provide email and password!', 400));
+    }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+  
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password', 401));
+    }
+  
+    // 3) If everything ok, send token to client
+    createSendToken(user, 200, res);
+});
+
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+      expires: new Date(Date.now() + 10 * 1000),
+      httpOnly: true
+    });
+    res.status(200).json({ status: 'success' });
+};
+
+// For protected routes (Authorized user can only access)
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+  
+    if (!token) {
+      return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+      );
+    }
+  
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(
+        new AppError(
+          'The user belonging to this token does no longer exist.',
+          401
+        )
+      );
+    }
+  
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next(
+        new AppError('User recently changed password! Please log in again.', 401)
+      );
+    }
+  
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    res.locals.user = currentUser;
+    next();
+});
