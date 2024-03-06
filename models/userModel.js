94affceaf14f06fe964862c621b68d48acfe6b40
Divya Prakash Jha <prakashdivya0607@gmail.com>
2024-03-07 00:19:02 +0530

    Profile updation, deletion and forgot password

--- models/userModel.js
+++ models/userModel.js
@@ -87,6 +87,21 @@ userSchema.methods.correctPassword = async function(candidatePassword, userPassw
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+  
+    this.passwordResetToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+  
+    console.log({ resetToken }, this.passwordResetToken);
+  
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  
+    return resetToken;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,15 @@ router.post('/signup',authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
+router.post('forgotPassword', authController.forgotPassword);
+router.patch('resetPassword/:token', authController.resetPassword);
+
 // Protect all routes after this middleware
 router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 module.exports = router;
