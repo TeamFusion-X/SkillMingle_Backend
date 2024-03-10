3b11995cea38492980265dd0cae02073828c1261
arvindsrathore <arvindsingh_21289@aitpune.edu.in>
2024-03-10 18:28:47 +0530

    Merge branch 'main' of github.com:TeamFusion-X/SkillMingle_Backend into asr

---
+++ models/chatModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const chatMessageSchema = new mongoose.Schema({
+    sender: {type: mongoose.Schema.Types.ObjectId, ref: 'Sender'},
+    content: String,
+    chat: {type: mongoose.Schema.Types.ObjectId, ref: 'ChatId'}
+    }, { timestamps: true }
+);
+
+const chatSchema = new mongoose.Schema({
+    isGroupChat: {
+        type: Boolean,
+        default: false
+    },
+    participants: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}]
+    }
+    }, { timestamps: true }
+);
+
+export const Message = mongoose.model('Message', chatMessageSchema);
+export const Chat = mongoose.model('Chat', chatSchema);
