4168b358024edecfb9154af1e9b4d52f6380fc25
Divya Prakash Jha <prakashdivya0607@gmail.com>
2024-03-11 17:12:11 +0530

    Actions on request added

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -9,4 +9,7 @@ router.use(authController.protect);
 router.get('/', requestController.getRequests);
 router.route('/send/:skill/:username').get(requestController.sendRequest);
 
-
+router.route('/accept/:requestID/skillShare/:skill?').get(requestController.takeAction, requestController.skillShare);
+router.route('/accept/:requestID/teachFree').get(requestController.takeAction, requestController.teachFree);
+router.route('/accept/:requestID/teachPaid').get(requestController.takeAction, requestController.teachPaid);
+router.route('/reject/:requestID').get(requestController.reject);
