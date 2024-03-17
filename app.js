1601037e11b998551f1978bb4a63cccb341a14c0
Divya Prakash Jha <prakashdivya0607@gmail.com>
2024-03-17 17:35:02 +0530

    Minor modifications and additions

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
-import {AppError} from './utils/appError.js';
-import {globalErrorHandler} from './controllers/errorController.js';
+import { AppError } from "./utils/appError.js";
+import { globalErrorHandler } from "./controllers/errorController.js";
 
 dotenv.config({ path: "./config.env" });
 
 // Routes
-import {router as userRouter} from "./routes/userRoutes.js";
-import {router as requestRouter} from "./routes/requestRoutes.js";
-import {router as chatRouter} from "./routes/chatRoutes.js";
-import {router as reviewRouter} from "./routes/reviewRoutes.js";
-import {router as recommendRouter} from "./routes/recommendRoutes.js";
+import { router as userRouter } from "./routes/userRoutes.js";
+import { router as requestRouter } from "./routes/requestRoutes.js";
+import { router as chatRouter } from "./routes/chatRoutes.js";
+import { router as reviewRouter } from "./routes/reviewRoutes.js";
+import { router as recommendRouter } from "./routes/recommendRoutes.js";
 
 export const app = express();
 
@@ -25,37 +25,37 @@ export const app = express();
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 // Limit requests from same IP
 const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Serving static files
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Parsing to JSON
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
 // Routes
-app.use('/api/users', userRouter);
-app.use('/api/requests', requestRouter);
-app.use('/api/chats', chatRouter);
-app.use('/api/reviews', reviewRouter);
-app.use('/api/recommend', recommendRouter);
+app.use("/api/users", userRouter);
+app.use("/api/requests", requestRouter);
+app.use("/api/chats", chatRouter);
+app.use("/api/reviews", reviewRouter);
+app.use("/api/recommend", recommendRouter);
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+app.all("*", (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 app.use(globalErrorHandler);
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,5 @@
-export const catchAsync = fn => {
-    return (req, res, next) => {
-      fn(req, res, next).catch(next);
-    };
+export const catchAsync = (fn) => {
+	return (req, res, next) => {
+		fn(req, res, next).catch(next);
+	};
 };
-  
