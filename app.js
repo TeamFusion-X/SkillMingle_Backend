7817f941c97b2a55f95772aed28a175e4cc13b33
Divya Prakash Jha <prakashdivya0607@gmail.com>
2025-01-19 19:58:09 +0530

    test

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/api", limiter);
 // Middleware to set CORP headers
 app.use((req, res, next) => {
 	res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); 
-	res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+	res.setHeader('Cross-Origin-Opener-Policy', 'cross-origin');
 	res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 	next();
 });
