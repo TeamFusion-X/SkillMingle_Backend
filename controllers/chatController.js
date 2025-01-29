81807d900e140e9aba1267cbed692aa7f935cbbf
Divya Prakash Jha <prakashdivya0607@gmail.com>
2025-01-29 18:27:21 +0530

    populated getChat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,68 +1,87 @@
-import {User} from './../models/userModel.js';
-import {Chat} from './../models/chatModel.js';
-import {catchAsync} from './../utils/catchAsync.js';
+import { User } from "./../models/userModel.js";
+import { Chat } from "./../models/chatModel.js";
+import { catchAsync } from "./../utils/catchAsync.js";
 
 export const getTeachingChats = catchAsync(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
+	const user = await User.findById(req.user.id);
 
-    const chatPromise = user.teachingConversations.map(async chatID => {
+	const chatPromise = user.teachingConversations.map(async (chatID) => {
 		const chat = await Chat.findById(chatID).populate("participants");
 		const id = chat._id;
 		const chatTitle = chat.chatTitle;
-		const chatWith = (req.user.id == chat.participants[0]._id) ? chat.participants[1].username : chat.participants[0].username;
+		const chatWith =
+			req.user.id == chat.participants[0]._id
+				? chat.participants[1].username
+				: chat.participants[0].username;
 
-		return {id, chatTitle, chatWith};
+		return { id, chatTitle, chatWith };
 	});
 
 	const chats = await Promise.all(chatPromise);
 
 	let allTeachingChats = [];
-	allTeachingChats.push(...chats)
+	allTeachingChats.push(...chats);
 
-    res.status(200).json({
-        status : "success",
-        teachingChats : allTeachingChats
-    })
+	res.status(200).json({
+		status: "success",
+		teachingChats: allTeachingChats,
+	});
 });
 
 export const getLearningChats = catchAsync(async (req, res, next) => {
 	const user = await User.findById(req.user.id);
-	
-    const chatPromise = user.learningConversations.map(async chatID => {
+
+	const chatPromise = user.learningConversations.map(async (chatID) => {
 		const chat = await Chat.findById(chatID).populate("participants");
 		const id = chat._id;
 		const chatTitle = chat.chatTitle;
-		const chatWith = (req.user.id == chat.participants[0]._id) ? chat.participants[1].username : chat.participants[0].username;
-		
-		return {id, chatTitle, chatWith};
+		const chatWith =
+			req.user.id == chat.participants[0]._id
+				? chat.participants[1].username
+				: chat.participants[0].username;
+
+		return { id, chatTitle, chatWith };
 	});
-	
+
 	const chats = await Promise.all(chatPromise);
 
 	let allLearningChats = [];
-	allLearningChats.push(...chats)
-	
-    res.status(200).json({
-		status : "success",
-        learningChats : allLearningChats
-    })  
+	allLearningChats.push(...chats);
+
+	res.status(200).json({
+		status: "success",
+		learningChats: allLearningChats,
+	});
 });
 
 export const getChat = catchAsync(async (req, res, next) => {
     const chat = await Chat.findById(req.params.chatId);
-	console.log(chat);
+    if (!chat) {
+        return next(new AppError('No chat found with this ID', 404));
+    }
+
+    const populatedChat = await Chat.findById(req.params.chatId)
+        .populate({
+            path: "messages",
+            select: "content sender createdAt"  
+        })
+        .populate({
+            path: "participants",
+            select: "name displayPicture"  
+        });
 
     res.status(200).json({
-		status: "success",
-		chat: chat
-	})
-})
+        status: "success",
+        chat: populatedChat,
+    });
+});
 
 export const increaseSkillPercentage = catchAsync(async (req, res, next) => {
-	const chat = await Chat.findByIdAndUpdate(req.params.chatId, {skillProgress : skillProgress + 10});
+	const chat = await Chat.findByIdAndUpdate(req.params.chatId, {
+		skillProgress: skillProgress + 10,
+	});
 
 	res.status(200).json({
-		status : "success"
-	})
-})
-
+		status: "success",
+	});
+});
